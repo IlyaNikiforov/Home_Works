be4f237bae2151c776337e52de490a0ae4e8c7d1
IlyaNikiforov <ilya.niki@gmail.com>
2014-05-26 18:27:36 +0400

    corrected 6.3

--- semester_2/home_work_6/task_3/sharedpointertest.h
+++ semester_2/home_work_6/task_3/sharedpointertest.h
@@ -14,35 +14,27 @@ class SharedPointerTest : public QObject
 
 	private slots:
 
-		void init()
-		{
-			pointer = new SharedPointer<int> (new int(10));
-		}
-
-		void cleanup()
-		{
-			delete pointer;
-		}
-
 		void testCopy()
 		{
-			SharedPointer<int> newPointer(*pointer);
+			SharedPointer<int> pointer(new int (10));
+			SharedPointer<int> newPointer(pointer);
 			QVERIFY(newPointer.getCount() == 2);
 			QVERIFY(*newPointer.getPointer() == 10);
-			delete newPointer;
-			QVERIFY(pointer->getCount() == 1);
+			QVERIFY(pointer.getCount() == 2);
 		}
 
 		void testAssignment()
 		{
-			SharedPointer<int> newPointer(new int(5));
-			newPointer = *pointer;
-			QVERIFY(pointer->getCount() == 2);
-			QVERIFY(*pointer->getPointer() == 10);
-			QVERIFY(newPointer.getCount() == 2);
-			QVERIFY(*newPointer.getPointer() == 5);
+			SharedPointer<int> pointer(new int (10));
+			SharedPointer<int> newPointer1(new int(9));
+			SharedPointer<int> newPointer2(newPointer1);
+			QVERIFY(newPointer2.getCount() == 2);
+			QVERIFY(newPointer1.getCount() == 2);
+			newPointer1 = pointer;
+			QVERIFY(newPointer2.getCount() == 1);
+			QVERIFY(newPointer1.getCount() == 2);
+			QVERIFY(pointer.getCount() == 2);
+			QVERIFY(*newPointer1.getPointer() == 10);
 		}
 
-	private:
-		SharedPointer<int> *pointer;
 };
